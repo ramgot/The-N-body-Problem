ca7ad924ec594ccc08140dfdd68f1c9c0043c178
ramgot <oleg.ramazanov.14@mail.ru>
2026-04-11 14:31:53 +0300

    [DPLM-5] много фиксов

--- Common/src/Vector3.cpp
+++ Common/src/Vector3.cpp
@@ -15,6 +15,10 @@ Vector3 Vector3::operator-(const Vector3& other) const {
     return Vector3(x - other.x, y - other.y, z - other.z);
 }
 
+Vector3 Vector3::operator-() const {
+    return Vector3(-x, -y, -z);
+}
+
 Vector3 Vector3::operator*(double scalar) const {
     return Vector3(x * scalar, y * scalar, z * scalar);
 }
